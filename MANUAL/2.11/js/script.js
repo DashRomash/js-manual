ae6e58cca3eeeb870a5f0b28b65bc167f11649f7
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-07-18 00:13:56 +0300

    Add files via upload

--- MANUAL/2.11/js/script.js
+++ MANUAL/2.11/js/script.js
@@ -1,5 +1,25 @@
-let age = 20;
-if (age >= 14 && age <= 90)
-    alert("Ok");
-else (alert"go home");
+//if (age >= 14 && age <= 90)
 
+//if (!(age > 14 && age < 90)) if (age > 14 || age < 90);
+
+//if (-1 || 0) alert('first') //-1 истина 0-ложно, должно вывести  истину
+//if (-1 && 0) alert('second'); //-1 истина 0-ложно, должно вывести ложность
+let userName = prompt("Кто там?", '');
+
+if (userName === 'Админ') {
+
+    let pass = prompt('Пароль?', '');
+
+    if (pass === 'Я главный') {
+        alert('Здравствуйте!');
+    } else if (pass === '' || pass === null) {
+        alert('Отменено');
+    } else {
+        alert('Неверный пароль');
+    }
+
+} else if (userName === '' || userName === null) {
+    alert('Отменено');
+} else {
+    alert("Я вас не знаю");
+}
