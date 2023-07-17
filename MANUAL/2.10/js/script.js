418f2d026249738d03391618b6d1b42d43bc8c35
DashRomash <118935565+DashRomash@users.noreply.github.com>
2023-07-18 01:42:09 +0300

    Add files via upload

---
+++ MANUAL/2.10/js/script.js
@@ -0,0 +1,28 @@
+'use strict';
+//let company = prompt("Какое «официальное» название JavaScript?", " ");
+//if (company == "ECMAScript") {
+//  alert("Верно!")
+//}
+//else {
+//   alert("Не знаете? ECMAScript!")
+//} OK!
+
+
+//let a = prompt('Введи число!');
+//if (a > 0) {
+//alert('1')
+//}
+//else if (a < 0) {
+//alert('-1')
+///}
+//else if (a == 0) {
+//alert('0')
+//} OK!
+
+
+let message;
+message = (login == 'Сотрудник') ? 'Привет' :
+    (login == 'Директор') ? 'Здравствуйте' :
+        (login == ' ') ? 'Нет логина' :
+            ' ';
+
